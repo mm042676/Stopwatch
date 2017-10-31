a1a2e98b8820c91431407aee129998c17002bdf4
Michael Matthews <mm042676@gmail.com>
2017-10-31 13:45:39 -0400

    daily

--- Stopwatch/Stopwatch/viewmodel/StopwatchViewModel.cs
+++ Stopwatch/Stopwatch/viewmodel/StopwatchViewModel.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
-namespace Stopwatch.viewmodel
+namespace Stopwatch
 {
-    class StopwatchViewModel
+    public class StopwatchViewModel : INotifyPropertyChanged
     {
+        string TimerTime = "0:00";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnStartButtonClicked(object sender, EventArgs args)
+        {
+            TimerTime = "1:00";
+        }
     }
 }
