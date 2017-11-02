bdee664604801ffa82b081234a803d6e066a490f
Michael Matthews <mm042676@gmail.com>
2017-11-02 13:38:39 -0400

    changed from shared project to PCL in visual studio

--- Stopwatch/Stopwatch/App.xaml.cs
+++ Stopwatch/Stopwatch/App.xaml.cs
@@ -7,28 +7,28 @@ using Xamarin.Forms;
 
 namespace Stopwatch
 {
-	public partial class App : Application
-	{
-		public App ()
-		{
-			InitializeComponent();
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
 
-			MainPage = new Stopwatch.view.SimpleStopwatch();
-		}
+            MainPage = new Stopwatch.MainPage();
+        }
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
