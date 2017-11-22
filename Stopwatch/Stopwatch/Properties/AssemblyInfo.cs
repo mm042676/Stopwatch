c20f6bb9ef2e132d162de073bfdac1100d10e01c
Michael Matthews <mm042676@gmail.com>
2017-11-22 15:54:02 -0500

    working on google play publishing requirements

--- Stopwatch/Stopwatch/Properties/AssemblyInfo.cs
+++ Stopwatch/Stopwatch/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Stopwatch")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("MikeNeedsWork")]
 [assembly: AssemblyProduct("Stopwatch")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
